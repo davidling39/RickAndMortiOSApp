5cbb20e83c2999d30f3fa0d2f76707414820b582
david ling <davidling39@davids-MacBook-Pro.local>
2023-01-31 18:58:52 +0800

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,8 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by david ling on 1/31/23.
+//
+
+import Foundation
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,8 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by david ling on 1/31/23.
+//
+
+import Foundation
