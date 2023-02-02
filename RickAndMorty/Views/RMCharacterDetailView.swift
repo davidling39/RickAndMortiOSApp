68c6ea3dc6059403b129056f653833d93bc11bdd
david ling <davidling39@davids-MacBook-Pro.local>
2023-02-02 16:22:24 +0800

    Character Detail Screen

---
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RMCharacterDetailView.swift
+//  RickAndMorty
+//
+//  Created by david ling on 2/2/23.
+//
+
+import UIKit
+
+class RMCharacterDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
